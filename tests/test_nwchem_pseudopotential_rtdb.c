52590edabad616d8958188474e19856f50908ed8
Rohit Goswami <r95g10@gmail.com>
2026-06-26 11:56:41 -0500

    test(pseudopotential): require text rtdb parity

--- tests/test_nwchem_pseudopotential_rtdb.c
+++ tests/test_nwchem_pseudopotential_rtdb.c
@@ -5,6 +5,7 @@
 #include <cmocka.h>
 
 extern void nwchemc_test_pseudopotential_rtdb(int *result);
+extern void nwchemc_test_pseudopotential_text_rtdb(int *result);
 
 static void test_pseudopotential_entries_reach_rtdb(void **state) {
   (void)state;
@@ -13,9 +14,17 @@ static void test_pseudopotential_entries_reach_rtdb(void **state) {
   assert_int_equal(result, 0);
 }
 
+static void test_pseudopotential_text_entries_reach_rtdb(void **state) {
+  (void)state;
+  int result = -1;
+  nwchemc_test_pseudopotential_text_rtdb(&result);
+  assert_int_equal(result, 0);
+}
+
 int main(void) {
   const struct CMUnitTest tests[] = {
       cmocka_unit_test(test_pseudopotential_entries_reach_rtdb),
+      cmocka_unit_test(test_pseudopotential_text_entries_reach_rtdb),
   };
   return cmocka_run_group_tests(tests, NULL, NULL);
 }
