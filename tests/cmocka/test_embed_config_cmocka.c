17c1a41687f3b7826f0615ab4695ea49b319545a
Rohit Goswami <r95g10@gmail.com>
2026-06-27 01:00:29 -0500

    test(nwpw): cover tetrahedron grid

--- tests/cmocka/test_embed_config_cmocka.c
+++ tests/cmocka/test_embed_config_cmocka.c
@@ -18,6 +18,7 @@ static const char *g_nwpw_spin_mode_path = NULL;
 static const char *g_nwpw_allow_translation_path = NULL;
 static const char *g_nwpw_cutoff_alias_path = NULL;
 static const char *g_nwpw_mc_steps_path = NULL;
+static const char *g_brillouin_tetrahedron_path = NULL;
 static const char *g_compact_cells_path = NULL;
 static const char *g_force_step_a_path = NULL;
 static const char *g_force_step_b_path = NULL;
@@ -2179,6 +2180,30 @@ static void test_embed_config_promotes_nwpw_mc_steps(void **state) {
   free(message);
 }
 
+static void test_embed_config_promotes_brillouin_tetrahedron(void **state) {
+  (void)state;
+  reset_embed_captures();
+  size_t message_size = 0;
+  unsigned char *message =
+      read_file(g_brillouin_tetrahedron_path, &message_size);
+  assert_non_null(message);
+
+  double pos[3] = {0.0, 0.0, 0.0};
+  int z[1] = {1};
+  double grad[3] = {0.0, 0.0, 0.0};
+  NWChemCResult result =
+      nwchemc_energy_gradient(1, pos, z, message, message_size, grad);
+
+  assert_int_equal(result.ok, 1);
+  assert_null(strstr(g_input_blocks, "  tetrahedron 4 5 6 tetraA\n"));
+  assert_int_equal(g_set_rtdb_values_calls, 1);
+  assert_int_equal(g_typed_set_count, 1);
+  assert_typed_set_triple("band:tetrahedron-grid",
+                          NWCHEMC_DIRECT_SET_VALUE_INTEGER, "4", "5", "6");
+
+  free(message);
+}
+
 static void test_session_reuses_config_across_geometry_steps(void **state) {
   (void)state;
   reset_embed_captures();
@@ -3623,12 +3648,13 @@ static void test_calculate_frequencies_one_shot_accepts_force_input(
 }
 
 int main(int argc, char **argv) {
-  if (argc != 16) {
+  if (argc != 17) {
     fprintf(stderr,
             "usage: %s PARAMS_BIN CONFIG_OPTIONS_BIN PSPSPIN_PARAMS_BIN "
             "PSPSPIN_MANY_PARAMS_BIN NWPW_SPIN_MODE_PARAMS_BIN "
             "NWPW_ALLOW_TRANSLATION_PARAMS_BIN NWPW_CUTOFF_ALIAS_PARAMS_BIN "
-            "NWPW_MC_STEPS_PARAMS_BIN FORCE_STEP_A_BIN FORCE_STEP_B_BIN FORCE_STEP_EV_BIN "
+            "NWPW_MC_STEPS_PARAMS_BIN BRILLOUIN_TETRAHEDRON_PARAMS_BIN "
+            "FORCE_STEP_A_BIN FORCE_STEP_B_BIN FORCE_STEP_EV_BIN "
             "FORCE_STEP_CHANGED_SPECIES_BIN "
             "FORCE_STEP_STATE_BIN TCE_METHODS_BIN COMPACT_CELLS_BIN\n",
             argv[0]);
@@ -3642,13 +3668,14 @@ int main(int argc, char **argv) {
   g_nwpw_allow_translation_path = argv[6];
   g_nwpw_cutoff_alias_path = argv[7];
   g_nwpw_mc_steps_path = argv[8];
-  g_force_step_a_path = argv[9];
-  g_force_step_b_path = argv[10];
-  g_force_step_ev_path = argv[11];
-  g_force_step_changed_species_path = argv[12];
-  g_force_step_state_path = argv[13];
-  g_tce_methods_path = argv[14];
-  g_compact_cells_path = argv[15];
+  g_brillouin_tetrahedron_path = argv[9];
+  g_force_step_a_path = argv[10];
+  g_force_step_b_path = argv[11];
+  g_force_step_ev_path = argv[12];
+  g_force_step_changed_species_path = argv[13];
+  g_force_step_state_path = argv[14];
+  g_tce_methods_path = argv[15];
+  g_compact_cells_path = argv[16];
   const struct CMUnitTest tests[] = {
       cmocka_unit_test(test_embed_config_uses_direct_dft_values),
       cmocka_unit_test(test_embed_config_promotes_compact_simulation_cells),
@@ -3660,6 +3687,7 @@ int main(int argc, char **argv) {
       cmocka_unit_test(test_embed_config_promotes_nwpw_allow_translation),
       cmocka_unit_test(test_embed_config_promotes_nwpw_cutoff_alias),
       cmocka_unit_test(test_embed_config_promotes_nwpw_mc_steps),
+      cmocka_unit_test(test_embed_config_promotes_brillouin_tetrahedron),
       cmocka_unit_test(test_session_reuses_config_across_geometry_steps),
       cmocka_unit_test(test_session_reapplies_after_one_shot_config),
       cmocka_unit_test(test_session_rejects_param_replacement_after_topology),
