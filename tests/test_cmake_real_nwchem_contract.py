2c41f1e7e780be7eea7c3e17c076972807513955
Rohit Goswami <r95g10@gmail.com>
2026-06-27 19:18:08 -0500

    test(build): cover cmake parser fixtures

--- tests/test_cmake_real_nwchem_contract.py
+++ tests/test_cmake_real_nwchem_contract.py
@@ -120,6 +120,111 @@ CMOCKA_FIXTURES = [
     ),
 ]
 
+CAPNP_PARSER_FIXTURES = [
+    ("nwchem_parser_params_bin", "NWCHEMC_PARSER_PARAMS_BIN"),
+    ("nwchem_pspspin_params_bin", "NWCHEMC_PSPSPIN_PARAMS_BIN"),
+    ("nwchem_nwpw_spin_mode_params_bin", "NWCHEMC_NWPW_SPIN_MODE_PARAMS_BIN"),
+    (
+        "nwchem_nwpw_allow_translation_params_bin",
+        "NWCHEMC_NWPW_ALLOW_TRANSLATION_PARAMS_BIN",
+    ),
+    (
+        "nwchem_nwpw_cutoff_alias_params_bin",
+        "NWCHEMC_NWPW_CUTOFF_ALIAS_PARAMS_BIN",
+    ),
+    ("nwchem_nwpw_mc_steps_params_bin", "NWCHEMC_NWPW_MC_STEPS_PARAMS_BIN"),
+    (
+        "nwchem_nwpw_bo_steps_default_params_bin",
+        "NWCHEMC_NWPW_BO_STEPS_DEFAULT_PARAMS_BIN",
+    ),
+    (
+        "nwchem_nwpw_bo_time_step_default_params_bin",
+        "NWCHEMC_NWPW_BO_TIME_STEP_DEFAULT_PARAMS_BIN",
+    ),
+    (
+        "nwchem_nwpw_bo_fake_mass_default_params_bin",
+        "NWCHEMC_NWPW_BO_FAKE_MASS_DEFAULT_PARAMS_BIN",
+    ),
+    (
+        "nwchem_nwpw_scaling_default_params_bin",
+        "NWCHEMC_NWPW_SCALING_DEFAULT_PARAMS_BIN",
+    ),
+    (
+        "nwchem_nwpw_np_dimensions_default_params_bin",
+        "NWCHEMC_NWPW_NP_DIMENSIONS_DEFAULT_PARAMS_BIN",
+    ),
+    (
+        "nwchem_nwpw_tolerances_default_params_bin",
+        "NWCHEMC_NWPW_TOLERANCES_DEFAULT_PARAMS_BIN",
+    ),
+    (
+        "nwchem_nwpw_mc_steps_default_params_bin",
+        "NWCHEMC_NWPW_MC_STEPS_DEFAULT_PARAMS_BIN",
+    ),
+    (
+        "nwchem_brillouin_tetrahedron_params_bin",
+        "NWCHEMC_BRILLOUIN_TETRAHEDRON_PARAMS_BIN",
+    ),
+    (
+        "nwchem_brillouin_dos_grid_params_bin",
+        "NWCHEMC_BRILLOUIN_DOS_GRID_PARAMS_BIN",
+    ),
+    ("nwchem_nwpw_et_params_bin", "NWCHEMC_NWPW_ET_PARAMS_BIN"),
+    (
+        "nwchem_nwpw_temperature_params_bin",
+        "NWCHEMC_NWPW_TEMPERATURE_PARAMS_BIN",
+    ),
+    (
+        "nwchem_nwpw_dos_default_params_bin",
+        "NWCHEMC_NWPW_DOS_DEFAULT_PARAMS_BIN",
+    ),
+    (
+        "nwchem_nwpw_mapping_alias_params_bin",
+        "NWCHEMC_NWPW_MAPPING_ALIAS_PARAMS_BIN",
+    ),
+    (
+        "nwchem_nwpw_mapping_default_params_bin",
+        "NWCHEMC_NWPW_MAPPING_DEFAULT_PARAMS_BIN",
+    ),
+    (
+        "nwchem_nwpw_virtual_alias_params_bin",
+        "NWCHEMC_NWPW_VIRTUAL_ALIAS_PARAMS_BIN",
+    ),
+    (
+        "nwchem_nwpw_one_electron_guess_defaults_params_bin",
+        "NWCHEMC_NWPW_ONE_ELECTRON_GUESS_DEFAULTS_PARAMS_BIN",
+    ),
+    (
+        "nwchem_nwpw_fractional_orbitals_default_params_bin",
+        "NWCHEMC_NWPW_FRACTIONAL_ORBITALS_DEFAULT_PARAMS_BIN",
+    ),
+    (
+        "nwchem_nwpw_smear_orbitals_default_params_bin",
+        "NWCHEMC_NWPW_SMEAR_ORBITALS_DEFAULT_PARAMS_BIN",
+    ),
+    (
+        "nwchem_nwpw_virtual_orbitals_default_params_bin",
+        "NWCHEMC_NWPW_VIRTUAL_ORBITALS_DEFAULT_PARAMS_BIN",
+    ),
+    (
+        "nwchem_nwpw_translate_vector_default_params_bin",
+        "NWCHEMC_NWPW_TRANSLATE_VECTOR_DEFAULT_PARAMS_BIN",
+    ),
+    (
+        "nwchem_nwpw_cell_expand_default_params_bin",
+        "NWCHEMC_NWPW_CELL_EXPAND_DEFAULT_PARAMS_BIN",
+    ),
+    (
+        "nwchem_brillouin_monkhorst_default_params_bin",
+        "NWCHEMC_BRILLOUIN_MONKHORST_DEFAULT_PARAMS_BIN",
+    ),
+    (
+        "nwchem_brillouin_dos_grid_default_params_bin",
+        "NWCHEMC_BRILLOUIN_DOS_GRID_DEFAULT_PARAMS_BIN",
+    ),
+    ("nwchem_compact_cells_params_bin", "NWCHEMC_COMPACT_CELLS_PARAMS_BIN"),
+]
+
 
 class CMakeRealNWChemContractTest(unittest.TestCase):
     maxDiff = None
@@ -149,6 +254,17 @@ class CMakeRealNWChemContractTest(unittest.TestCase):
         )
         self.assert_contains_all(
             command, [variable for _target, variable in CMOCKA_FIXTURES]
+        )
+
+    def test_capnp_parser_receives_all_parser_fixture_arguments(self):
+        dependencies = self._block(r"add_dependencies\(\s*test_capnp_parser\b")
+        command = self._block(r"add_test\(\s*NAME\s+capnp-parser\b")
+
+        self.assert_contains_all(
+            dependencies, [target for target, _variable in CAPNP_PARSER_FIXTURES]
+        )
+        self.assert_contains_all(
+            command, [variable for _target, variable in CAPNP_PARSER_FIXTURES]
         )
 
     def test_generates_rgpot_potential_config_and_cell_fixtures(self):
